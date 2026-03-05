6913d2b08be947934ee94773ec940f7ab7b63770
Ullbergm <ullbergm@arcada.fi>
2026-03-05 15:15:18 +0200

    Changes

--- model_comments.php
+++ model_comments.php
@@ -2,9 +2,10 @@
 // Task: Fetch "karlssoj" as comment sender
 // Task2: Fetch karlssojs bio as the comment "Hello"
 
-//Hämta användardata från databasen
- $sql = "SELECT * FROM profiles WHERE username = ?";
- $stmt = $conn->prepare($sql); //SQL injection protection
- $stmt ->execute([$username]); //Klistra in bobby tables i förberedda C-koden
- $row = $result->fetch(); // Hämta PDOStatementens data och lagra i en PHP variabel
+$userid = $_SESSION['user_id'];
+//Fetch data from database
+ $sql = "SELECT sender_id comment, reciver_id, username FROM profiles INNER JOIN comments ON profiles.id = comments.sender_id";
+ $stmt = $conn->prepare($sql); 
+ $stmt ->execute();
+ $row = $stmt->fetch(); 
  // print_r($row);
--- view_comments.php
+++ view_comments.php
@@ -1,7 +1,7 @@
 <?php include "model_comments.php"; ?>
 
-<h3> User <?= $row[`username`] ?> commented:</h3>
-<p><?= $row[`bio`] ?></p>
+<h3> User <?= $row['sender_id'] ?> commented:</h3>
+<p><?= $row['comment'] ?></p>
 <form action="profile.php" method="GET">
         Your reply: <input type="text" name="reply" placeholder="Your reply here"><br>
         <input type="submit" value="Reply to user"><br>
